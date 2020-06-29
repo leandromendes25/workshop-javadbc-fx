ca47a11395ac010e330b505dbb5b29da0e702e5c
leandromendes25 <leandromiendes@yahoo.com>
2020-06-29 19:52:55 -0300

    initializing action as parameter

---
+++ src/gui/DepartmentListController.java
@@ -0,0 +1,69 @@
+package gui;
+
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import model.entities.Department;
+import model.services.DepartmentService;
+
+public class DepartmentListController implements Initializable {
+
+	private DepartmentService service;
+	// Instanciando a tableView
+	@FXML
+	private TableView<Department> tableViewDepartment;
+	@FXML
+	// O tipo da entidade e depois o tipo da coluna
+	private TableColumn<Department, Integer> tableColumnId;
+	@FXML
+	private TableColumn<Department, String> tableColumnName;
+	@FXML
+	private Button btNew;
+	private ObservableList<Department> obsList;
+
+	@FXML
+	public void onBtNewAction() {
+		System.out.println("butom");
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
+	}
+
+	public void initializeNodes() {
+		// para iniciar os comportamento das colunas
+		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+		// window pega referencia do stage, é uma super classe
+		Stage stage = (Stage) Main.getMainScene().getWindow();
+		// tableView Acompanhar o tamanho da janela
+		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
+	}
+
+	public void updateTableView() {
+		if (service == null) {
+			throw new IllegalStateException("service is null");
+		}
+		List<Department> list = service.findAll();
+		obsList = FXCollections.observableArrayList(list);
+		// setItems para carregar na view
+		tableViewDepartment.setItems(obsList);
+
+	}
+}
---
+++ src/gui/MainViewerController.java
@@ -0,0 +1,76 @@
+package gui;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.function.Consumer;
+
+import application.Main;
+import gui.util.Alerts;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+import model.services.DepartmentService;
+
+public class MainViewerController implements Initializable {
+	@FXML
+	private MenuItem menuItemSeller;
+	@FXML
+	private MenuItem menuItemDepartment;
+	@FXML
+	private MenuItem menuItemAbout;
+
+	@FXML
+	public void onMenuSellerAction() {
+		System.out.println("onMenuSellerAction");
+	};
+
+	@FXML
+	public void onMenuDepartmentAction() {
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+		});
+	};
+
+	@FXML
+	public void onMenuAboutAction() {
+		// como o about não leva a nada
+		loadView("/gui/About.fxml", x -> {
+		});
+	};
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+
+	}
+
+	public synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			// vai retornar o controlador do tipo que chamar
+			T controller = loader.getController();
+			// usamos o consumer
+			initializingAction.accept(controller);
+		} catch (IOException e) {
+			Alerts.showAlert("IOException", "ERROR loading view", e.getMessage(), AlertType.ERROR);
+			;
+		}
+
+	}
+}
